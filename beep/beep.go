01055f1539b71aa8b5224c99dd436aeebd2f67b9
Andreas Jonsson <mail@andreasjonsson.se>
2018-11-19 16:16:56 +0100

    Just a backup.

--- beep/beep.go
+++ beep/beep.go
@@ -6,7 +6,37 @@
 package beep
 
 import (
+	"os"
+	"syscall"
 	"time"
 )
 
-func Beep(f int, s time.Duration) {}
+const kdKIOCSOUND = 0x4B2F
+
+var commandChan = make(chan func() time.Duration, 256)
+
+func init() {
+	go func() {
+		f := <-commandChan
+		for {
+			t := time.NewTimer(f())
+			select {
+			case <-t:
+				syscall.Syscall(syscall.SYS_IOCTL, os.Stdout.Fd(), uintptr(kdKIOCSOUND), uintptr(0))
+			case f = <-commandChan:
+			}
+			t.Stop()
+		}
+	}()
+}
+
+func Beep(f int, d time.Duration) {
+	commandChan <- func() time.Duration {
+		var freq uintptr
+		if f > 0 {
+			freq = uintptr(1193180 / f)
+		}
+		syscall.Syscall(syscall.SYS_IOCTL, os.Stdout.Fd(), uintptr(kdKIOCSOUND), freq)
+		return d
+	}
+}
