428f7e045a314cf190d7899c9163e2968da2bdbe
Andreas T Jonsson <mail@andreasjonsson.se>
2018-11-23 17:51:40 +0100

    Updated build.

--- tools/package/itch/deploy.sh
+++ tools/package/itch/deploy.sh
@@ -5,6 +5,8 @@ unzip butler.zip
 chmod +x butler
 ./butler -V
 
+export USERVERSION=${BASE_VERSION}-${CI_PIPELINE_ID}
+
 ./butler login
-./butler push den.zip phix/den:edge --userversion ${BASE_VERSION}-${CI_PIPELINE_ID}
-./butler push densrv.zip phix/den:edge --userversion ${BASE_VERSION}-${CI_PIPELINE_ID}
+./butler push den.zip phix/den:windows-osx-linux-edge --userversion $USERVERSION
+./butler push densrv.zip phix/den:windows-osx-linux-server-edge --userversion $USERVERSION
