b34fd9e2d5e5b5f4383107fe15c0ca480a45f870
Andreas T Jonsson <mail@andreasjonsson.se>
2018-11-26 16:32:44 +0100

    Just a backup.

--- tools/package/snap/deploy.sh
+++ tools/package/snap/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# snapcraft login
+# snapcraft export-login snapcraft.login
+# base64 snapcraft.login | xsel --clipboard
+
 echo $SNAPCRAFT_LOGIN_FILE | base64 --decode --ignore-garbage > ./../../../.snapcraft/snapcraft.cfg
 
 snapcraft build
