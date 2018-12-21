d49d62c939d41b13b91ef3a1a2982ab03e874b73
Andreas T Jonsson <mail@andreasjonsson.se>
2018-12-21 09:42:01 +0100

    Fixed invalid version variable.

--- tools/package/itch/release.sh
+++ tools/package/itch/release.sh
@@ -7,7 +7,7 @@ go generate
 tools/package/itch/build.sh
 
 $BUTLER_DIR/butler validate den
-$BUTLER_DIR/butler push den phix/den:windows-osx-linux --userversion $FULL_VERSION
+$BUTLER_DIR/butler push den phix/den:windows-osx-linux --userversion $BASE_VERSION
 
 $BUTLER_DIR/butler validate densrv
-$BUTLER_DIR/butler push densrv phix/den:server-windows-osx-linux --userversion $FULL_VERSION
+$BUTLER_DIR/butler push densrv phix/den:server-windows-osx-linux --userversion $BASE_VERSION
