93f8676fc42a3a72c809b91e410489b5a0326c6d
Andreas T Jonsson <mail@andreasjonsson.se>
2019-01-06 23:48:55 +0100

    Updated version.

--- version/version.go
+++ version/version.go
@@ -4,18 +4,18 @@
 package version
 
 const (
-	Major = 0
-	Minor = 0
+	Major = 1
+	Minor = 1
 	Patch = 0
 )
 
 var (
-	String = "0.0.0"
-	Full   = "0.0.0-0 (722999231cb5672d0b41234bac8b4e05b635b1fc)"
+	String = "1.1.0"
+	Full = "1.1.0-0 (7d9696c7f6c752c99bada0408d0d4913af1fd553)"
 )
 
 var (
-	Build     = 0
-	Hash      = "722999231cb5672d0b41234bac8b4e05b635b1fc"
-	Copyright = "Copyright (C) 2018 Andreas T Jonsson"
+	Build = 0
+	Hash = "7d9696c7f6c752c99bada0408d0d4913af1fd553"
+	Copyright = "Copyright (C) 2018-2019 Andreas T Jonsson"
 )
