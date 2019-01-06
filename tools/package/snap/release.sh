380607c6ae76c31e703a7525d750e657b9384fae
Andreas T Jonsson <mail@andreasjonsson.se>
2019-01-06 23:02:28 +0100

    Updated script.

--- tools/package/snap/release.sh
+++ tools/package/snap/release.sh
@@ -6,6 +6,7 @@ export FULL_VERSION=${BASE_VERSION}.0
 rpl e34f19fc-289d-4fb9-b134-c1d07a29a273 "${BASE_VERSION}-0" snapcraft.yaml
 
 go generate
-snapcraft cleanbuild
+#snapcraft cleanbuild
+snapcraft snap
 
 snapcraft push --release=stable den_${BASE_VERSION}-0_amd64.snap
