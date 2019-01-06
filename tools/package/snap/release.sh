f281d2c264cbd50a93b757cf0704e0cc18d68878
Andreas T Jonsson <mail@andreasjonsson.se>
2019-01-06 23:51:06 +0100

    Updated scripts.

--- tools/package/snap/release.sh
+++ tools/package/snap/release.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 export BASE_VERSION=1.1.0
-export FULL_VERSION=${BASE_VERSION}.0
 
-go generate
 snapcraft cleanbuild
-
 snapcraft push --release=stable den_${BASE_VERSION}-0_amd64.snap
