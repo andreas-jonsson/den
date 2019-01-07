00b26eca9a0d2e09d8c51314fed260871fd45742
Andreas T Jonsson <mail@andreasjonsson.se>
2019-01-07 12:22:47 +0100

    Changed license.

--- version/version.go
+++ version/version.go
@@ -1,21 +1,21 @@
-// DEN
+// DEN is released under GPL3
 // Copyright (C) 2018-2019 Andreas T Jonsson
 
 package version
 
 const (
 	Major = 1
-	Minor = 1
+	Minor = 2
 	Patch = 0
 )
 
 var (
-	String = "1.1.0"
-	Full = "1.1.0-0 (7d9696c7f6c752c99bada0408d0d4913af1fd553)"
+	String = "1.2.0"
+	Full   = "1.2.0-0 (93f8676fc42a3a72c809b91e410489b5a0326c6d)"
 )
 
 var (
-	Build = 0
-	Hash = "7d9696c7f6c752c99bada0408d0d4913af1fd553"
+	Build     = 0
+	Hash      = "93f8676fc42a3a72c809b91e410489b5a0326c6d"
 	Copyright = "Copyright (C) 2018-2019 Andreas T Jonsson"
 )
