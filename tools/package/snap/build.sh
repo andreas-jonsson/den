d506a3a662bb5bace29daf5f115fc7802d6031d5
Andreas T Jonsson <mail@andreasjonsson.se>
2019-01-07 22:58:59 +0100

    Updated version.

--- tools/package/snap/build.sh
+++ tools/package/snap/build.sh
@@ -6,7 +6,7 @@
 
 echo $SNAPCRAFT_LOGIN_FILE | base64 --decode --ignore-garbage > ./../../../.snapcraft/snapcraft.cfg
 
-export CURRENT_VERSION=1.1.0-0
+export CURRENT_VERSION=1.2.0-0
 rpl ${CURRENT_VERSION} "${BASE_VERSION}-${CI_PIPELINE_ID}" snapcraft.yaml
 
 go generate
